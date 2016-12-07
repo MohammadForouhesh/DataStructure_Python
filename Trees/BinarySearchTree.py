df7425a6e0db1897ab64814fe24090fee7a7b0bd
Mohammad H Forouhesh <Mohammad.Forouhesh@outlook.com>
2016-12-07 18:01:13 +0330

    BST inner position class is implemented

--- Trees/BinarySearchTree.py
+++ Trees/BinarySearchTree.py
@@ -3,3 +3,19 @@ from Trees.LinkedBinaryTree import LinkedBinaryTree
 from Trees.MapBase import MapBase
 
 
+class TreeMap(LinkedBinaryTree, MapBase):
+    """Sorted map implementation using a binary search tree."""
+
+    # ---------------------------- override Position class ----------------------------
+    class Position(LinkedBinaryTree.Position):
+        @property
+        def key(self):
+            """Return key of map's key-value pair."""
+            return self.element()._key
+
+        @property
+        def value(self):
+            """Return value of map's key-value pair."""
+            return self.element()._value
+
+    # ------------------------------- nonpublic utilities -------------------------------
