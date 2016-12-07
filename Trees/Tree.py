34d718d02b829ae169738e93a45c69f5809a9668
Mohammad H Forouhesh <Mohammad.Forouhesh@outlook.com>
2016-12-07 16:55:12 +0330

    Tree abstract class compeleted but not TDD

--- Trees/Tree.py
+++ Trees/Tree.py
@@ -61,3 +61,121 @@ class Tree(metaclass=ABCMeta):
         """
         raise NotImplementedError('must be implemented by subclass')
 
+    # ---------- concrete methods implemented in this class ----------
+    @property
+    def is_root(self, p):
+        """
+        Return True if Position p represents the root of the tree.
+        """
+        return self.root() == p
+
+    @property
+    def is_leaf(self, p):
+        """
+        Return True if Position p does not have any children.
+        """
+        return self.num_children(p) == 0
+
+    @property
+    def is_empty(self):
+        """
+        Return True if the tree is empty.
+        """
+        return len(self) == 0
+
+    @property
+    def depth(self, p):
+        """
+        Return the number of levels separating Position p from the root.
+        """
+        if self.is_root(p):
+            return 0
+        else:
+            return 1 + self.depth(self.parent(p))
+
+    def _height1(self):                                                             # works, but O(n^2) worst-case time
+        """
+        Return the height of the tree.
+        """
+        return max(self.depth(p) for p in self.positions() if self.is_leaf(p))
+
+    def _height2(self, p):                                                          # time is linear in size of subtree
+        """
+        Return the height of the subtree rooted at Position p.
+        """
+        if self.is_leaf(p):
+            return 0
+        else:
+            return 1 + max(self._height2(c) for c in self.children(p))
+
+    def height(self, p=None):
+        """
+        Return the height of the subtree rooted at Position p.
+        If p is None, return the height of the entire tree.
+        """
+        if p is None:
+            p = self.root()
+        return self._height2(p)                                                     # start _height2 recursion
+
+    def __iter__(self):
+        """
+        Generate an iteration of the tree's elements.
+        """
+        for p in self.positions():                                                  # use same order as positions()
+            yield p.element()                                                       # but yield each element
+
+    def positions(self):
+        """
+        Generate an iteration of the tree's positions.
+        """
+        return self.preorder()                                                      # return entire preorder iteration
+
+    def preorder(self):
+        """
+        Generate a preorder iteration of positions in the tree.
+        """
+        if not self.is_empty():
+            for p in self._subtree_preorder(self.root()):                           # start recursion
+                yield p
+
+    def _subtree_preorder(self, p):
+        """
+        Generate a preorder iteration of positions in subtree rooted at p.
+        """
+        yield p                                                                     # visit p before its subtrees
+        for c in self.children(p):                                                  # start recursion
+            for other in self._subtree_preorder(c):                                 # do preorder of c's subtree
+                yield other                                                         # yielding each to our caller
+
+    def postorder(self):
+        """
+        Generate a postorder iteration of positions in the tree.
+        """
+        if not self.is_empty():
+            for p in self._subtree_postorder(self.root()):                          # start recursion
+                yield p
+
+    def _subtree_postorder(self, p):
+        """
+        Generate a postorder iteration of positions in subtree rooted at p.
+        """
+        for c in self.children(p):                                                  # for each child c
+            for other in self._subtree_postorder(c):                                # do postorder of c's subtree
+                yield other                                                         # yielding each to our caller
+        yield p                                                                     # visit p after its subtrees
+
+    def breadthfirst(self):
+        """
+        Generate a breadth-first iteration of the positions of the tree.
+        """
+        if not self.is_empty():
+            fringe = LinkedQueue()                                                  # known positions not yet yielded
+            fringe.enqueue(self.root())                                             # starting with the root
+            while not fringe.is_empty():
+                p = fringe.dequeue()                                                # remove from front of the queue
+                yield p                                                             # report this position
+                for c in self.children(p):
+                    fringe.enqueue(c)                                               # add children to back of queue
+
+
+# not test
