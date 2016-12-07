e127d041e112754d0653097e6d32c33d4c632dd4
Mohammad H Forouhesh <Mohammad.Forouhesh@outlook.com>
2016-12-07 18:01:46 +0330

    BST subtree search is implemented

--- Trees/BinarySearchTree.py
+++ Trees/BinarySearchTree.py
@@ -19,3 +19,15 @@ class TreeMap(LinkedBinaryTree, MapBase):
             return self.element()._value
 
     # ------------------------------- nonpublic utilities -------------------------------
+    def _subtree_search(self, p, k):
+        """Return Position of p's subtree having key k, or last node searched."""
+        if k == p.key:                                                      # found match
+            return p
+        elif k < p.key:                                                     # search left subtree
+            if self.left(p) is not None:
+                return self._subtree_search(self.left(p), k)
+        else:                                                               # search right subtree
+            if self.right(p) is not None:
+                return self._subtree_search(self.right(p), k)
+        return p                                                            # unsucessful search
+
